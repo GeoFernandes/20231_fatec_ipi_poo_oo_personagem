993d7070feb6c566418c0ac610b4e7b6d7023980
AlineGe <geovanna.oliveira01@fatec.sp.gov.br>
2023-03-09 16:26:21 -0300

    Apostila02(OO): bloco de código 2.3.1 encerrado.

--- Personagem.java
+++ Personagem.java
@@ -1,21 +1,47 @@
+import static java.lang.Math.min;
 public class Personagem {
-    //variáveis de instância
+    //variáveis de instância são globais. Retornam o valor 0.
     String nome;
-    int energia;
-    int fome;
-    int sono;
+    int energia = 10;
+    int fome = 0;
+    int sono = 0;
 
         //isso é um método de instância
     void cacar () {
-        System.out.println(nome + " cacando");
+        if (energia >= 2) {
+            System.out.println(nome + " cacando");
+            //energia = energia - 2;
+            energia -= 2;
+        }
+        else {
+            System.out.println(nome + " sem energia para cacar");
+        }
+        fome = min(fome + 1,10);
+        sono = min(sono + 1, 10);
+    }
+
+    //\n = pular linha no formato print. 
+    void comer () {
+        if (fome >= 1) {
+            System.out.print(nome + " comendo\n");
+            energia = min (energia + 1, 10);
+            //fome = fome - 1; fome--; --fome;
+            fome -= 1;
+        }
+        else {
+            System.out.println(nome + " sem fome");
+        }
     }
 
     //%n = pular linha independente da plataforma. 
-    void comer () {
-        System.out.print(nome + " comendo%n");
-    }
-
     void dormir () {
-        System.out.printf("%s dormindo%n");
+        if (sono >= 1) {
+            System.out.printf("%s dormindo%n", nome);
+            energia = energia + 1 < 10 ? energia + 1 : 10;
+            sono--;
+        }
+        else {
+            System.out.println(nome + " sem sono");
+        }
     }
 }
--- TestePersonagem.java
+++ TestePersonagem.java
@@ -1,12 +1,31 @@
 public class TestePersonagem {
     static public void main (String [] args) {
+        /*int idade = 18;
+        if (idade >= 18) {
+            System.out.println("Sim, pode dirigir");
+        }
+        else {
+            System.out.println("Não, não pode dirigir");
+        }
+
+                            =
+
+        System.out.println(idade >= 18 ? "Sim" : "Não");*/
+
         //p é uma variável de referência. Faz referência ao objeto construído com new.
         //Personagem p = new Personagem ();
-        //java 10+
+
+        //variáveis declaradas no método main, são variáveis locais.
+        //variáveis locais não inicializadas não são compiladas.
+
+        //java 10+ (interferência de tipo)
         var p = new Personagem ();
+
+        System.out.println(p.energia);
 
         //nome do objeto referenciado por p
         p.nome = "John";
+        //Estado do objeto (coleção de valores) = 10, 0, 0, John
         p.cacar();
         p.comer();
         p.dormir();
